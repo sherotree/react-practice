b9b009e6f3bb7139655fff78ab61aa34324563da
sherotree <wytreeshero@163.com>
2019-11-08 16:09:17 +0800

    helloworld

---
+++ demo1/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { directive } from '@babel/types'
+//import React from 'react'
+//const Component = React.Component
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+export default App
