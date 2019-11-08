53b7c5576941e5595a1afc4223dc46dae1446919
sherotree <wytreeshero@163.com>
2019-11-08 17:46:23 +0800

    add this

--- demo2/src/Menu.js
+++ demo2/src/Menu.js
@@ -1,16 +1,34 @@
 import React, { Component,Fragment } from 'react'
 
 class Menu extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '菜单',
+      list:[]
+    }
+  } 
+
   render() {
     return (
       <Fragment>
-        <div> <input /> <button>增加服务</button></div>
+        <div>
+          <input value={this.state.inputValue} onChange={this.inputChange.bind(this)} />
+          <button>增加服务</button>
+        </div>
         <ul>
           <li>糖醋排骨</li>
           <li>宫保鸡丁</li>
         </ul>
       </Fragment>
     )
+  }
+  inputChange(e) {
+    console.log(this)
+    this.setState({
+      inputValue:e.target.value
+    })
+
   }
 }
 
