392dd8bb9fb623095ac250cfce0ed1856ea1b48d
sherotree <wytreeshero@163.com>
2019-11-08 16:36:48 +0800

    jsx

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 import { directive } from '@babel/types'
+import { unlink } from 'fs'
 //import React from 'react'
 //const Component = React.Component
 
 class App extends Component{
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <ul className='my-list'>
+        <li>{false?'11111':'55555'}</li>
+        <li>22222</li>
+      </ul>
     )
+    // var child1 = React.createElement('li', null, '33333')
+    // var child2 = React.createElement('li', null, '44444')
+    // var root = React.createElement('ul',{className:'my-list'},child1,child2)
   }
 }
 
