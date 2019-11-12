2f61ca1f0e87057cfffa4c5f3506bc4b954ce00e
sherotree <wytreeshero@163.com>
2019-11-12 11:24:29 +0800

    try02

--- demo03/src/index.js
+++ demo03/src/index.js
@@ -4,20 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+// const user = {
+//   firstName: 'harper',
+//   lastName: 'Perez'
+// };
+
+// const element = (
+//   <h1>
+//     Hello,{formatName(user)}
+//   </h1>
+// );
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello,World!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(element,document.getElementById('root'))
 }
-const user = {
-  firstName: 'harper',
-  lastName: 'Perez'
-};
 
-const element = (
-  <h1>
-    Hello,{formatName(user)}
-  </h1>
-);
-
-ReactDOM.render(element, document.getElementById('root'));
+setInterval(tick,1000)
 
 
